53107ace525091746cbc22d759a2fbc6e1f27cf6
shaunl <shaun.lawless@gmail.com>
2013-01-17 23:14:44 +0000

    Day6 solution

---
+++ Day6/src/RecentList.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * PHP version 5.4
+ *
+ * @category
+ * @package
+ * @author      Shaun Lawless
+ * @version     1.0
+ * @created     17/01/13 - 22:00
+ * @copyright   2013 - Shaun Lawless
+ */
+
+/**
+ *
+ */
+class RecentList
+{
+
+}
+/**
+ * End of file: RecentList.php
+ */
